559c3ae4173f687c6980595b8de4dada979fcd9a
YarynkaShcherban <yaryna.shcherban@gmail.com>
2025-11-14 17:32:20 +0200

    Yaryna.shcherban (#1)
    * Added all view files. Now you an do urls.py (its simple)
    
    * final touches
    
    * added update for commands put and patch in baseview.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,7 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
